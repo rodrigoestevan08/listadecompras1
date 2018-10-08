e01fa7888846f1f1d32420d8254d19ce94835ca7
Renan Rodrigo <renanrb@ime.usp.br>
2018-10-08 09:17:09 -0300

    Construir o endpoint get de produtos

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(mongoDB("mongodb://localhost/lista"));
 
+app.get('/produtos', (req, res) => {
+  req.db.collection('compras').find().toArray((erro, dados) => {
+    if(!erro){
+      res.send({produtos: dados});
+    }
+    return res.status(500).send({erro: erro});
+  })
+});
+
+// app.post();
+
+// app.delete();
+
 app.listen(5000, erro => {
   if(!erro){
     return console.log("Aplicação de backend iniciada");
